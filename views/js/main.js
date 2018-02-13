961cd99b3b43d6d265005db760ad5982465ce3a9
mahmmad3007 <34791790+mahmmad3007@users.noreply.github.com>
2018-02-13 21:57:15 +0300

    fix441error

--- views/js/main.js
+++ views/js/main.js
@@ -438,7 +438,7 @@ var resizePizzas = function(size) {
     }
   }
 
-  changePizzaSizes(size);
+  //changePizzaSizes(size);
 
   // User Timing API is awesome
   window.performance.mark("mark_end_resize");
