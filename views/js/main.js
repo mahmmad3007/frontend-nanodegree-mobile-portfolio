c859dd7b5bf28c84488d0c251970aa842a1503f8
mahmmad3007 <34791790+mahmmad3007@users.noreply.github.com>
2018-02-13 22:08:41 +0300

    restorebackup

--- views/js/main.js
+++ views/js/main.js
@@ -1,3 +1,23 @@
+/*
+Welcome to the 60fps project! Your goal is to make Cam's Pizzeria website run
+jank-free at 60 frames per second.
+
+There are two major issues in this code that lead to sub-60fps performance. Can
+you spot and fix both?
+
+
+Built into the code, you'll find a few instances of the User Timing API
+(window.performance), which will be console.log()ing frame rate data into the
+browser console. To learn more about User Timing API, check out:
+http://www.html5rocks.com/en/tutorials/webperformance/usertiming/
+
+Creator:
+Cameron Pittman, Udacity Course Developer
+cameron *at* udacity *dot* com
+*/
+
+// As you may have realized, this website randomly generates pizzas.
+// Here are arrays of all possible pizza ingredients.
 var pizzaIngredients = {};
 pizzaIngredients.meats = [
   "Pepperoni",
@@ -402,10 +422,10 @@ var resizePizzas = function(size) {
   changeSliderLabel(size);
 
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
- // function determineDx (elem, size) {
-//    var oldWidth = elem.offsetWidth;
-//    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-//    var oldSize = oldWidth / windowWidth;
+  function determineDx (elem, size) {
+    var oldWidth = elem.offsetWidth;
+    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+    var oldSize = oldWidth / windowWidth;
 
     // Changes the slider value to a percent width
     function sizeSwitcher (size) {
@@ -420,18 +440,16 @@ var resizePizzas = function(size) {
           console.log("bug in sizeSwitcher");
       }
     }
-// no need to culclate new size in this funcation 
-   // var newSize = sizeSwitcher(size);
-   // var dx = (newSize - oldSize) * windowWidth;
+
+    var newSize = sizeSwitcher(size);
+    var dx = (newSize - oldSize) * windowWidth;
 
     return dx;
   }
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    var pizzaSize = document.getElementById("randomPizzaContainer");
-    
-    for (var i = 0; i < pizzaSize.length; i++) {
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
