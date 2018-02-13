dbacaefdd9d14a94b5f1b8e3ccecd55aa60c88a3
mahmmad3007 <34791790+mahmmad3007@users.noreply.github.com>
2018-02-13 21:52:09 +0300

    fixerrer

--- views/js/main.js
+++ views/js/main.js
@@ -445,7 +445,7 @@ var resizePizzas = function(size) {
   window.performance.measure("measure_pizza_resize", "mark_start_resize", "mark_end_resize");
   var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
   console.log("Time to resize pizzas: " + timeToResize[timeToResize.length-1].duration + "ms");
-};
+
 
 window.performance.mark("mark_start_generating"); // collect timing data
 
