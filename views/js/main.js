280a8c497feb52bc9d0db8f1b85e2406f8698b9c
mahmmad3007 <34791790+mahmmad3007@users.noreply.github.com>
2018-02-16 00:23:04 +0300

    CorrectSubmision error

--- views/js/main.js
+++ views/js/main.js
@@ -454,15 +454,11 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  var phase = [];
-
-  for (var i = 0; i < 5; i++) {
-    phase.push(Math.sin(scrollTop / 1250 + i) * 100);
-  }
+ var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     // document.body.scrollTop is no longer supported in Chrome.
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
